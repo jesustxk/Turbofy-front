b0336a24cdc1c7e109c79902d7ced6dae30a66d1
Jesús Checa <jesuscheca11@gmail.com>
2023-06-17 11:33:18 +0200

    feat: Search song page

--- src/app/pages/search-songs/search-songs.page.ts
+++ src/app/pages/search-songs/search-songs.page.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Song } from '../../models/song';
 import { TurbofyApiService } from '../../services/turbofy-api.service';
 
 @Component({
@@ -9,14 +10,32 @@ import { TurbofyApiService } from '../../services/turbofy-api.service';
   templateUrl: './search-songs.page.html',
   styleUrls: ['./search-songs.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class SearchSongsPage implements OnInit {
 
-  constructor(public turbofyApi: TurbofyApiService) { }
+  searchForm: FormGroup | undefined;
 
-  ngOnInit() {
-    
+  songs: Song[] = [];
+
+  constructor(public turbofyApi: TurbofyApiService, private formBuilder: FormBuilder) { }
+
+  async ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      name: new FormControl('', Validators.compose([
+        Validators.pattern('[a-z A-Z]+')
+      ])),
+      artist: new FormControl('', Validators.compose([
+        Validators.pattern('[a-z A-Z]+')
+      ])),
+      date: new FormControl('', Validators.compose([
+        Validators.pattern('[0-9]{4}/[0-1]{0,1}[1-9]{1}/[0-3]{0,1}[1-9]{1}')
+      ])),
+    });
+  }
+
+  async searchSong(value: { name: String; artist: String; date: String }) {
+    this.songs = await this.turbofyApi.searchSong(value.name, value.artist, value.date);
   }
 
 }
--- src/app/services/turbofy-api.service.ts
+++ src/app/services/turbofy-api.service.ts
@@ -30,8 +30,27 @@ export class TurbofyApiService {
     return songs;
   }
 
-  async searchSong() {
+  async searchSong(name: String, artist: String, date: String): Promise<Song[]> {
+    console.log('WORK')
+    let queryParams = '';
 
+    if (name) {
+      queryParams += 'name=' + name;
+    } else if (artist) {
+      queryParams += 'artist=' + artist;
+    } else if (date) {
+      queryParams += 'date=' + date;
+    }
+
+    let responseSongs = await fetch(this.turbofyAPI + '/songs/search?' + queryParams, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const songs = await responseSongs.json();
+    return songs;
   }
 
   async updateSong() {
