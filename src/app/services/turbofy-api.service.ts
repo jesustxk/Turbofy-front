ddede2d82ae1d69a056d16ba1b4f3e5ec990103b
Jesús Checa <jesuscheca11@gmail.com>
2023-06-17 23:23:12 +0200

    feat: Styles

--- src/app/services/turbofy-api.service.ts
+++ src/app/services/turbofy-api.service.ts
@@ -11,14 +11,18 @@ export class TurbofyApiService {
   constructor() { }
 
   async addSong() {
+    console.log('addSong');
 
   }
 
   async getSong(songId: string) {
+    console.log('getSong');
 
   }
 
   async getAllSongs(): Promise<Song[]> {
+    console.log('getAllSongs');
+    
     let responseSongs = await fetch(this.turbofyAPI + '/songs/readAll', {
       method: 'GET',
       headers: {
@@ -31,7 +35,8 @@ export class TurbofyApiService {
   }
 
   async searchSong(name: String, artist: String, date: String): Promise<Song[]> {
-    console.log('WORK')
+    console.log('searchSong');
+    
     let queryParams = '';
 
     if (name) {
@@ -54,22 +59,27 @@ export class TurbofyApiService {
   }
 
   async updateSong() {
+    console.log('updateSong');
 
   }
 
   async deleteSong() {
+    console.log('deleteSong');
 
   }
 
   async getSpotySongs() {
+    console.log('getSpotySongs');
 
   }
 
   async addComment() {
+    console.log('addComment');
 
   }
 
   async deleteComment() {
+    console.log('deleteComment');
     
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDwU6NR687GYR4bevsYreldHkc5O6KoGLM",
+    authDomain: "jcg577-dah2023.firebaseapp.com",
+    projectId: "jcg577-dah2023",
+    storageBucket: "jcg577-dah2023.appspot.com",
+    messagingSenderId: "1005786466876",
+    appId: "1:1005786466876:web:42c92b2ff904b8e2981139",
+    measurementId: "G-2SLQEN930F"
+  }
 };
